bfbea560015ec8d9d1f3993b904fe3f609ce45e8
Gowtham <30896453+Prince-Gowtham@users.noreply.github.com>
2022-04-08 12:17:41 +0530

    Update copy.h

--- System Calls/copy/copy.h
+++ System Calls/copy/copy.h
@@ -1,13 +1,29 @@
 /******************************************/
-/* Author	: Gowtham S               */
-/* Date		: 08-apr-2022             */
-/* Filename	: copy.h                  */
+/* Author	: Gowtham S                     */
+/* Date		: 08-apr-2022                   */
+/* Filename	: copy.h                      */
 /* Description	: copy file to other file */
 /******************************************/
 
+#ifndef COPY_H
+#define COPY_H
+
+
+/* Included header files */
 
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
+
+
+/* Macro Definitions */
+
+
+/* user defined datatype definitions */
+
+
+/* function prototypes */
+
+#endif /* COPY_H */
