37162804c2a61644f0ecf18e1298c62c6ec63af9
Gowtham <30896453+Prince-Gowtham@users.noreply.github.com>
2022-04-08 12:11:44 +0530

    Copy function

---
+++ System Calls/copy/copy.c
@@ -0,0 +1,42 @@
+/****************************************************************/
+/*   Author      : Gowtham.S					*/
+/*   Date        : 08-04-2022					*/
+/*   Filename    : copy.c					*/
+/*   Description : copying file to another file  		*/
+/*								*/
+/****************************************************************/
+
+/* Included header file */
+
+#include "copy.h"
+
+/* Global variable definitions */
+
+/* function definitions */
+
+int main(int argc,char *argv[])
+{
+    int fd,fd1,fd2,fd3,fd4;
+    char a[4665313];
+
+    /*opening file*/
+    fd=open(argv[1],O_RDONLY);
+    (fd==-1)?printf("file not opened\n"):printf("file opened\n");
+
+    /*reading file */
+    fd1=read(fd,a,4665313);
+    (fd1==-1)?printf("file not readed\n"):printf("file readed\n");
+
+    /*creat file */
+    fd2=creat(argv[2],S_IRWXU | S_IRWXG | S_IRWXO);
+    (fd2==-1)?printf("file not creat\n"):printf("file created\n");
+    
+    /*write file */
+    fd3=write(fd2,a,4665313);
+    (fd3==-1)?printf("file not write\n"):printf("file writed\n");
+
+    close(fd);
+    close(fd1);
+    close(fd2);
+    close(fd3);
+}
---
+++ System Calls/copy/copy.h
@@ -0,0 +1,13 @@
+/******************************************/
+/* Author	: Gowtham S               */
+/* Date		: 08-apr-2022             */
+/* Filename	: copy.h                  */
+/* Description	: copy file to other file */
+/******************************************/
+
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
