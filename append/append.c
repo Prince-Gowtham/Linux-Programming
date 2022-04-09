6a73e1308f8c7404551d6e7a82d058bb8d2b0f8a
Gowtham <30896453+Prince-Gowtham@users.noreply.github.com>
2022-04-09 19:28:01 +0530

    Append
    Joining two files into single file

---
+++ append/append.c
@@ -0,0 +1,69 @@
+/**************************************************************************/
+/*   Author      : Gowtham						  */
+/*   Date        : 09-04-2022						  */
+/*   Filename    : append.c						  */
+/*   Description : reading 2 files and writing in single file             */
+/*									  */
+/**************************************************************************/
+
+/* Included header file */
+
+#include "append.h"
+
+/* Global variable definitions */
+
+/* function definitions */
+
+
+int main(int argc,char* argv[])
+{
+    //Initilation
+
+    char buf[buf_size];
+    int inputFd,inputFd1,outputFd,inputFlag,outputFlag;
+    ssize_t numRead;
+    mode_t mode;
+
+    //Declaration
+
+    inputFlag  = O_RDONLY;
+    outputFlag = O_CREAT | O_RDONLY | O_WRONLY | O_APPEND;
+    mode       = S_IRUSR | S_IWUSR  | S_IRGRP  | S_IWGRP | S_IROTH;
+
+    //Opening File
+
+    inputFd = open(argv[1],inputFlag);
+    if(inputFd == -1)
+    {
+	printf("%s : Error creating file",argv[1]);
+	exit(-10);
+    }
+
+    inputFd1 = open(argv[2],inputFlag);
+    if(inputFd1 == -1)
+    {
+	printf("%s : Error creating file",argv[1]);
+	exit(-10);
+    }
+
+    outputFd = open(argv[3],outputFlag,mode);
+    if(outputFd == -1)
+    {
+	printf("%s : Error creating file",argv[2]);
+	exit(-20);
+    }
+
+    //Reading file1 and writing file
+
+    while((numRead=read(inputFd,buf,buf_size))>0)
+	if((write(outputFd,buf,numRead))!=numRead);
+
+    //Reading file2 and writing file the same file
+
+    while((numRead=read(inputFd1,buf,buf_size))>0)
+	if((write(outputFd,buf,numRead))!=numRead);
+    
+    close(inputFd);
+    close(outputFd);
+    exit(0);
+}
---
+++ append/append.h
@@ -0,0 +1,33 @@
+/****************************************************************/
+/*   Author      : Gowtham.S					*/
+/*   Date        : 09-04-2022					*/
+/*   Filename    : append.h					*/
+/*   Description : reading 2 files,writing in single file      	*/
+/*								*/
+/****************************************************************/
+
+
+
+#ifndef	APPEND_H
+#define APPEND_H
+
+
+/* Included header files */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Macro Definitions */
+
+#define buf_size 1024
+
+/* user defined datatype definitions */
+
+
+/* function prototypes */
+
+#endif /* APPEND_H */
