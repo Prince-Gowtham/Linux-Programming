270df0399d8d988bb265617d15eeb95f7f2f8daf
Gowtham <30896453+Prince-Gowtham@users.noreply.github.com>
2022-04-14 14:53:08 +0530

    Update tail.c

--- assignment_1_10-04-2022/tail/tail.c
+++ assignment_1_10-04-2022/tail/tail.c
@@ -21,19 +21,19 @@ int main(int argc,char *argv[])
     char a,*b = argv[1];
     struct stat statFd;
     
-    i=atoi(argv[2]);
-    inputFd = open(b,O_RDONLY);
-    stat(b, &statFd);
+    i=atoi(argv[2]);//atoi command it is used to convert string to interger
+    inputFd = open(b,O_RDONLY);//opening the file
+    stat(b, &statFd);//predefined function have in this structure file status
 
-    while(lseek(inputFd,count, SEEK_SET) < statFd.st_size)
+    while(lseek(inputFd,count, SEEK_SET) < statFd.st_size)//checing the file and moving the offset using lseek command
     {
-        if(read(inputFd,&a,1) != '\n')
+        if(read(inputFd,&a,1) != '\n')//reading the filediscriptor byte by byte
         {
-            printf("%c",a);
+            printf("%c",a);//printing the buffer
             count++;
         }
 	j++;
-	if(j == i)
+	if(j == i)//when this line reaches the exact conditions was true the break statement executed
 	    break;
     }
     close(inputFd);
